7e17120ee3b8cf8ace88342aecf3e18cda310b5a
SadAwoo <46636256+SadAwoo@users.noreply.github.com>
2019-02-03 23:40:37 -0500

    Code for Challenge Roll a Ball

---
+++ PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text scoreText;
+    public Text livesText;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+    private int lives;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        lives = 3;
+        SetCountText();
+        SetScoreText();
+        SetLivesText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+  
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText();
+            SetLivesText();
+            SetScoreText();
+        }
+        if (other.gameObject.CompareTag("BPick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score - 1;
+            lives = lives - 1;
+            SetCountText();
+            SetLivesText();
+            SetScoreText();
+        }
+    }
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 12) 
+{
+    transform.position = new Vector3(72.993f, transform.position.y,3.0f); 
+}
+    }
+    void SetScoreText ()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (count >= 24)
+        {
+            winText.text = "You Win!";
+        }
+        if ( lives == 0)
+        {
+            Destroy(this.gameObject);
+            winText.text = "You Lost!";
+        }
+    }
+}
---
+++ verticalmovin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//idk what Im doing, coding is a hell, I cant figure out how to get these to not start in the middle of the course
+public class verticalmovin : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position.Set(70, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the wall from 74.14, -0.012, 0 to 71.49, -0.012, 0
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * speed, 8));
+    }
+}
